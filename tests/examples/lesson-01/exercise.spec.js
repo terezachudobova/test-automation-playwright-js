f90979d8300285ecbad72bb9b29f1d53f84dce7a
Zaneta <zaneta@alfred.is>
2024-09-25 13:17:12 +0200

    put settings.json, package update, lesson-5

--- tests/examples/lesson-01/exercise.spec.js
+++ tests/examples/lesson-01/exercise.spec.js
@@ -1,21 +1,19 @@
-const {test} = require("@playwright/test");
+const { test } = require("@playwright/test");
 
 /**
  * Lesson 1: First test
  */
-test('should open login page', async ({ page }) => {
+test("should open login page", async ({ page }) => {
+  await page.goto("/prihlaseni");
 
-    await page.goto('/prihlaseni');
+  await page.setViewportSize({ width: 1920, height: 1080 });
 
-    await page.setViewportSize({ width: 1920, height: 1080 })
+  await page.screenshot({ path: "login_page.png" });
 
-    await page.screenshot({ path: 'login_page.png' });
+  const pageTitleSelector = page.locator("css=h1");
 
-    const pageTitleSelector = page.locator('css=h1');
-
-    console.log('TEST');
-    console.log(await page.title());
-    console.log(await pageTitleSelector.textContent());
-    // await page.pause(5000);
-
+  console.log("TEST");
+  console.log(await page.title());
+  console.log(await pageTitleSelector.textContent());
+  // await page.pause(5000);
 });
--- tests/examples/lesson-05/example-assertions.spec.js
+++ tests/examples/lesson-05/example-assertions.spec.js
@@ -1,41 +1,35 @@
 // TODO
 
 import { expect, test } from "@playwright/test";
+import { password, username } from "../../fixtures";
 
 test.describe("Assertion Examples", () => {
-  test("TODO", async ({ page }) => {
-    //visibility vs. hidden
+  test("visibility, attached in DOM, toHaveValue, ", async ({ page }) => {
+    await page.goto("/");
+    //visibility vs. tobeAttached
     await expect(page.getByText("Více informací")).toBeVisible();
     await page.getByText("Více informací").click();
-    await expect(page.getByText("Více informací")).hidden();
+    await expect(page.getByText("Více informací")).not.toBeAttached();
 
-    await expect(locator).toBeAttached(); //Element is attached
-    await expect(locator).toBeChecked(); //Checkbox is checked
-    await expect(locator).toBeDisabled(); //Element is disabled
-    await expect(locator).toBeEditable(); //Element is editable
-    await expect(locator).toBeEmpty(); //Container is empty
-    await expect(locator).toBeEnabled(); //Element is enabled
-    await expect(locator).toBeFocused(); //Element is focused
-    await expect(locator).toBeHidden(); //Element is not visible
-    await expect(locator).toBeInViewport(); //Element intersects viewport
-    await expect(locator).toBeVisible(); //Element is visible
-    await expect(locator).toContainText(); //Element contains text
-    await expect(locator).toHaveAccessibleDescription(); //Element has a matching accessible description
-    await expect(locator).toHaveAccessibleName(); //Element has a matching accessible name
-    await expect(locator).toHaveAttribute(); //Element has a DOM attribute
-    await expect(locator).toHaveClass(); //Element has a class property
-    await expect(locator).toHaveCount(); //List has exact number of children
-    await expect(locator).toHaveCSS(); //Element has CSS property
-    await expect(locator).toHaveId(); //Element has an ID
-    await expect(locator).toHaveJSProperty(); //Element has a JavaScript property
-    await expect(locator).toHaveRole(); //Element has a specific ARIA role
-    await expect(locator).toHaveScreenshot(); //Element has a screenshot
-    await expect(locator).toHaveText(); //Element matches text
-    await expect(locator).toHaveValue(); //Input has a value
-    await expect(locator).toHaveValues(); //Select has options selected
-    await expect(page).toHaveScreenshot(); //Page has a screenshot
-    await expect(page).toHaveTitle(); //Page has a title
-    await expect(page).toHaveURL(); //Page has a URL
-    await expect(response).toBeOK(); //Response has an OK status
+    // fill in value and check it is filled
+    await page.getByText("Pro učitelé").click();
+    await page.getByText("Objednávka pro MŠ/ZŠ").click();
+    await expect(page.getByLabel("IČO")).toBeEmpty();
+    await page.getByLabel("IČO").fill("12345678");
+    await expect(page.getByLabel("IČO")).toHaveValue("12345678");
+  });
+
+  test("checkbox element is checked", async ({ page }) => {
+    await page.goto("/");
+    await page.getByRole("button", { name: "Pro rodiče" }).click();
+    await page.getByText("Vytvořit přihlášku").click();
+    await page.getByLabel("Email").fill(username);
+    await page.getByLabel("Heslo").fill(password);
+    await page.getByRole("button", { name: "Přihlásit" }).click();
+    await page.getByRole("link", { name: " Přihlášky" }).click();
+    await page.getByText("Přidat").click();
+    // await page.locator("#restrictions_yes").check(); //explain why this does not work in our case
+    await page.locator("#restrictions_yes").check({ force: true });
+    await expect(page.locator("#restrictions_yes")).toBeChecked();
   });
 });
