54277a685b1b6d01a3f67640ccb611a42deff0f0
monikaprotivova <monika.protivova@cleverfarm.ag>
2024-10-02 07:22:55 +0200

    update first example

--- src/examples/01-basics/exercise.spec.js
+++ src/examples/01-basics/exercise.spec.js
@@ -5,15 +5,15 @@ import { test } from "@playwright/test";
  */
 test("should open login page", async ({ page }) => {
   await page.goto("/prihlaseni");
+  console.log(await page.title());
+
+  console.log("This is my first test!");
+
+  await page.pause(5000);
+
+  await page.setViewportSize({ width: 800, height: 600 });
+  await page.screenshot({ path: "login_page_800_600.png" });
 
   await page.setViewportSize({ width: 1920, height: 1080 });
-
-  await page.screenshot({ path: "login_page.png" });
-
-  const pageTitleSelector = page.locator("h1");
-
-  console.log("TEST");
-  console.log(await page.title());
-  console.log(await pageTitleSelector.textContent());
-  // await page.pause(5000);
+  await page.screenshot({ path: "login_page_1920_1080.png" });
 });
